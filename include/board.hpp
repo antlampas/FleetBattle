6b8b27102e801cdd1259bdf4930cd6ed9d8e1529
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-03-21 17:29:39 +0100

    -

--- include/board.hpp
+++ include/board.hpp
@@ -57,7 +57,7 @@ class board
     decodedCoordinatesPair_t decodeCoordinates(coordinates_t);
     virtual bool             hit(coordinates_t)                            = 0; 
     public:
-    enum shootReturnCodes {HIT,MISSED,SQUARE_ALREADY_HIT};
+    enum shootReturnCodes    {HIT,MISSED,SQUARE_ALREADY_HIT};
     public:
     board();
     virtual ~board();
--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,5 @@
 
 int main(int argc,char** argv)
 {
-    deployedShips_t ds {};
-
-    ds.push_back(std::pair<std::string,std::string>{"a1","a3"});
-    ds.push_back(std::pair<std::string,std::string>{"c1","g1"});
-    ds.push_back(std::pair<std::string,std::string>{"b3","b5"});
-    ds.push_back(std::pair<std::string,std::string>{"c3","c5"});
-    ds.push_back(std::pair<std::string,std::string>{"g6","i6"});
-    ds.push_back(std::pair<std::string,std::string>{"g10","j10"});
-    
-    playerBoard pb {ds};
-    opponentBoard ob;
-
-    board_t bs = pb.getBoardStatus();
-
-    for(auto row: bs){
-        for(auto column: row)
-            std::cout << column << " ";
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-
-    for(auto c: {"a1","b5","h6","b2","a3"})
-    {
-        pb.shoot(c);
-        board_t bs = pb.getBoardStatus();
-        for(auto row: bs){
-            for(auto column: row)
-                std::cout << column << " ";
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
-
     return -1;
 }
