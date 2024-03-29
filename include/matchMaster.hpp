519aaf8429377f91dc98c30a482fc4a72b2da257
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-03-29 10:39:03 +0100

    -

--- include/matchMaster.hpp
+++ include/matchMaster.hpp
@@ -29,8 +29,8 @@ namespace fleetBattle
             std::pair<board::playerInTurn_t,board::shipsSquaresList_t> ships;
             std::unique_ptr<playerBoard::playerBoard>                  playerBoardA;
             std::unique_ptr<playerBoard::playerBoard>                  playerBoardB;
-            std::shared_ptr<playerBoard::command_t>                    command;
-            board::playerInTurn_t                                      playerInTurn;
+            std::shared_ptr<command_t>                                 command;
+            playerInTurn_t                                             playerInTurn;
             std::shared_ptr<std::mutex>                                mutex;
 
             private:
