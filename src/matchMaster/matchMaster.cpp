2f86b2e019e5f7f4c171ccb1eb0f7359fd56267b
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-03-29 10:41:51 +0100

    -

--- src/matchMaster/matchMaster.cpp
+++ src/matchMaster/matchMaster.cpp
@@ -10,10 +10,10 @@ namespace fleetBattle
 {
     namespace matchMaster
     {
-        matchMaster::matchMaster(   std::unique_ptr<board::playerBoard> bA,
-                                    std::unique_ptr<board::playerBoard> bB,
-                                    std::shared_ptr<board::command_t>   cA,
-                                    board::playerInTurn_t               p
+        matchMaster::matchMaster(   std::unique_ptr<playerBoard::playerBoard> bA,
+                                    std::unique_ptr<playerBoard::playerBoard> bB,
+                                    std::shared_ptr<command_t>                cA,
+                                    playerInTurn_t                            p
                                 ) : playerBoardA(std::move(bA)),
                                     playerBoardB(std::move(bB)),
                                     command(std::make_shared<command_t>(std::make_pair("",""))),
