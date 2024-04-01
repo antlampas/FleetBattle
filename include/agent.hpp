9d1a37fa854e2fcb894914f954de59cb3a8bd7fd
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-01 17:16:50 +0200

    -

---
+++ include/agent.hpp
@@ -0,0 +1,23 @@
+/*
+ *
+ * FALP Fleet Battle © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+#include <memory>
+#include <iostream>
+#include <mutex>
+#include "types.hpp"
+
+ namespace fleetBattle
+ {
+    class agent final
+    {
+        private:
+        std::shared_ptr<std::mutex> mutex;
+
+        public:
+        agent();
+        void operator()();
+    }
+ }
