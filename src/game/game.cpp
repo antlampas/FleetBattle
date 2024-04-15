a4532d47c4c06d18ac649e8343221070ceecbfcb
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-15 16:52:14 +0200

    -

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -14,32 +14,32 @@ namespace fleetBattle
               deployedShips_t deployedA,
               deployedShips_t deployedB)  :   playerA{new player( std::shared_ptr<fleetBattle::playerBoard>(new fleetBattle::playerBoard(deployedA)),
                                                                   std::shared_ptr<fleetBattle::opponentBoard>(new fleetBattle::opponentBoard()),
-                                                                  std::shared_ptr<command_t>(commandPtr),
-                                                                  std::shared_ptr<std::mutex>(mA))
+                                                                  commandPtr,
+                                                                  mA)
                                               },
                                               playerB{new player( std::shared_ptr<fleetBattle::playerBoard>(new fleetBattle::playerBoard(deployedB)),
                                                                   std::shared_ptr<fleetBattle::opponentBoard>(new fleetBattle::opponentBoard()),
-                                                                  std::shared_ptr<command_t>(commandPtr),
-                                                                  std::shared_ptr<std::mutex>(mB))
+                                                                  commandPtr,
+                                                                  mB)
                                               },
                                               mm{new matchMaster( this->playerA,
                                                                   this->playerB,
                                                                   std::shared_ptr<playerBoard>(playerA->ownBoard),
                                                                   std::shared_ptr<playerBoard>(playerB->ownBoard),
-                                                                  std::shared_ptr<std::mutex>(mA),
-                                                                  std::shared_ptr<std::mutex>(mB),
-                                                                  std::shared_ptr<command_t>(commandPtr),
+                                                                  mA,
+                                                                  mB,
+                                                                  commandPtr,
                                                                   'A')
                                               },
                                               agentA{new agent( 'A',
                                                                 this->mm->playerInTurn_public,
-                                                                std::shared_ptr<std::mutex>(mA),
-                                                                std::shared_ptr<command_t>(commandPtr))
+                                                                mA,
+                                                                commandPtr)
                                               },
                                               agentB{new agent( 'B',
                                                                 this->mm->playerInTurn_public,
-                                                                std::shared_ptr<std::mutex>(mB),
-                                                                std::shared_ptr<command_t>(commandPtr))
+                                                                mB,
+                                                                commandPtr)
                                               }
   {}
   game::~game(){}
