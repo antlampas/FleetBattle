d4147fb2d71e4996d9551b46eb8427fff025c099
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-16 13:46:28 +0200

    -

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -15,33 +15,33 @@ namespace fleetBattle
               deployedShips_t deployedB)
   {
     this->playerA = std::move(std::make_shared<player>      {
-                                                    new player  {       
-                                                                  std::shared_ptr<playerBoard>   { new playerBoard(deployedA) },
-                                                                  std::shared_ptr<opponentBoard> { new opponentBoard()        },
-                                                                  commandPtr,
-                                                                  mA
-                                                                }
-                                                  });
+                                                              new player  {       
+                                                                            std::shared_ptr<playerBoard>   { new playerBoard(deployedA) },
+                                                                            std::shared_ptr<opponentBoard> { new opponentBoard()        },
+                                                                            commandPtr,
+                                                                            mA
+                                                                          }
+                                                            });
     this->playerB = std::move(std::make_shared<player>      {
-                                                    new player  {
-                                                                  std::shared_ptr<playerBoard>   { new playerBoard(deployedB) },
-                                                                  std::shared_ptr<opponentBoard> { new opponentBoard() },
-                                                                  commandPtr,
-                                                                  mB
-                                                                }
-                                                  });
+                                                              new player  {
+                                                                            std::shared_ptr<playerBoard>   { new playerBoard(deployedB) },
+                                                                            std::shared_ptr<opponentBoard> { new opponentBoard() },
+                                                                            commandPtr,
+                                                                            mB
+                                                                          }
+                                                            });
     this->mm      = std::move(std::make_shared<matchMaster> {
-                                                    new matchMaster {
-                                                                      this->playerA,
-                                                                      this->playerB,
-                                                                      this->playerA->ownBoard,
-                                                                      this->playerB->ownBoard,
-                                                                      mA,
-                                                                      mB,
-                                                                      commandPtr,
-                                                                      'A'
-                                                                    }
-                                                  });
+                                                              new matchMaster {
+                                                                                this->playerA,
+                                                                                this->playerB,
+                                                                                this->playerA->ownBoard,
+                                                                                this->playerB->ownBoard,
+                                                                                mA,
+                                                                                mB,
+                                                                                commandPtr,
+                                                                                'A'
+                                                                              }
+                                                            });
     this->agentA = std::shared_ptr<agent>{ new agent('A',this->mm->playerInTurn_public,mA,commandPtr) };
     this->agentB = std::shared_ptr<agent>{ new agent('B',this->mm->playerInTurn_public,mB,commandPtr) };
   }
