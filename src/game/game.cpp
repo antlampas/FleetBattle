978d01673270ee8eef9a8dea386e87542c64da96
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-16 13:44:27 +0200

    -

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -14,23 +14,23 @@ namespace fleetBattle
               deployedShips_t deployedA,
               deployedShips_t deployedB)
   {
-    this->playerA = std::make_shared<player>      {
+    this->playerA = std::move(std::make_shared<player>      {
                                                     new player  {       
                                                                   std::shared_ptr<playerBoard>   { new playerBoard(deployedA) },
                                                                   std::shared_ptr<opponentBoard> { new opponentBoard()        },
                                                                   commandPtr,
                                                                   mA
                                                                 }
-                                                  };
-    this->playerB = std::make_shared<player>      {
+                                                  });
+    this->playerB = std::move(std::make_shared<player>      {
                                                     new player  {
                                                                   std::shared_ptr<playerBoard>   { new playerBoard(deployedB) },
                                                                   std::shared_ptr<opponentBoard> { new opponentBoard() },
                                                                   commandPtr,
                                                                   mB
                                                                 }
-                                                  };
-    this->mm      = std::make_shared<matchMaster> {
+                                                  });
+    this->mm      = std::move(std::make_shared<matchMaster> {
                                                     new matchMaster {
                                                                       this->playerA,
                                                                       this->playerB,
@@ -41,7 +41,7 @@ namespace fleetBattle
                                                                       commandPtr,
                                                                       'A'
                                                                     }
-                                                  };
+                                                  });
     this->agentA = std::shared_ptr<agent>{ new agent('A',this->mm->playerInTurn_public,mA,commandPtr) };
     this->agentB = std::shared_ptr<agent>{ new agent('B',this->mm->playerInTurn_public,mB,commandPtr) };
   }
