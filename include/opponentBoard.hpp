11b1b9a3d33f4af03a1f09470132cb3c9b59ca26
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-17 16:11:59 +0200

    -

--- include/opponentBoard.hpp
+++ include/opponentBoard.hpp
@@ -27,8 +27,7 @@ namespace fleetBattle
         virtual squareStatus_t getSquareStatus(coordinates_t)                override final;
         virtual squareStatus_t setSquareStatus(coordinates_t,squareStatus_t) override final;
         /* Not implemented and do not implement! */
-        virtual bool           isSquareAlreadyHit(coordinates_t)             override final;
-        virtual shootStatus_t  shoot(coordinates_t)                          override final;
+        virtual bool           isSquareAlreadyHit(coordinates_t)             override final = delete;
     };
 }
 #endif
