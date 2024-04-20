5e84640f4b40fc52a6e07f9e7017b448529a8141
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-04-20 12:59:40 +0200

    -

--- src/playerBoard/checkShipSunk.cpp
+++ src/playerBoard/checkShipSunk.cpp
@@ -17,25 +17,35 @@ namespace fleetBattle
         decodedCoordinatesPair_t decodedCoordinates = decodeCoordinates(coordinates);
         std::array<std::array<std::string,5>,5> deployedShipsMap {};
 
-        int j=0;
-        for(auto deployedShip: this->deployedShips)
+        int i=0;
+        bool shipHit = false;
+        bool shipSunk = false;
+        for(auto deployedShip: this->deployedShipsMap)
         {
-            if(isShipHorizontal(deployedShip))
+            for(auto square: deployedShip)
             {
-                char column1 = std::regex_match(deployedShip.first,c);
-                char column2 = std::regex_match(deployedShip.second,c);
-                char row     = std::regex_match(deployedShip.first,r);
-                for(char coord=column1,int i=0;coord<=column2;coord++,i++)
-                    deployedShipsMap.at(j).at(i) = std::string(coord+row);
+                if(coordinates == square)
+                {
+                    shipHit = true;
+                    break;
+                }
             }
-            if(isShipVertical(deployedShip))
+            if(shipHit)
             {
-                char row1   = std::regex_match(deployedShip.first,r);
-                char row2   = std::regex_match(deployedShip.second,r);
-                char column = std::regex_match(deployedShip.first,c);
-                for(char coord=row1,int i=0;coord<=row2;coord++,i++)
-                    deployedShipsMap.at(j).at(i) = std::string(col+coord);
+                for(auto square=deployedShip.begin();;std::advance(square))
+                {
+                    std::regex shipSquareHit("[s]");
+                    squareStatus_t shipSquare = this->getSquareStatus(square);
+                    if(!std::regex_match(shipSquare,shipSquareHit))
+                        break;
+                    else
+                        if(square==deployedShip.end())
+                            this->destroyedShip.at(i) = true;
+                }
+                return;
             }
+        i++;
         }
+        return;
     }
 }
