742e29b70a12f5ddec4a536405b78e2a1088b0d7
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-07 20:11:11 +0200

    -

--- src/agent/functor.cpp
+++ src/agent/functor.cpp
@@ -17,19 +17,24 @@ namespace fleetBattle
         asio::streambuf input {};
         asio::error_code error;
 
+        unsigned char playerInTurn = 'A';
+
         this->cli->accept(*this->socket);
 
         this->standalone = true;
 
         std::shared_lock lock(*this->serviceMutex);
-        
+
         while(true)
         {
-            unsigned char playerInTurn = 'A';
             std::string incomingMessage = *this->serviceChannel;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
             
             if(incomingMessage.size()-1 == 1)
                 playerInTurn = incomingMessage.at(0);
+            
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
             output = std::string(1,this->player) + std::string(": waiting for your turn...\nPlayer in turn: " + std::string(1,playerInTurn) + "\n");
             asio::write(*this->socket,asio::buffer(output.c_str(),output.size()),asio::transfer_at_least(output.size()),error);
@@ -67,7 +72,7 @@ namespace fleetBattle
                     break;
                 }
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(80));
         }
         return true;
     }
