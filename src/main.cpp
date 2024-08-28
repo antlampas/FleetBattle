d5d3b66e7aae7ae77899b59d8b06e19ada288c8e
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-28 16:48:09 +0200

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <memory>
 #include <thread>
 
@@ -9,7 +8,7 @@
 
 int main(int argc,char** argv)
 {
-    std::atomic<std::shared_ptr<fleetBattle::command_t>> cmd            { std::make_shared<fleetBattle::command_t>() };
+    std::shared_ptr<fleetBattle::command_t> cmd { std::make_shared<fleetBattle::command_t>() };
 
     fleetBattle::deployedShips_t deployedShipsA {std::make_pair("a1","a2"),std::make_pair("b1","b2"),std::make_pair("c1","c3"),std::make_pair("d1","d4"),std::make_pair("e1","e5")};
     fleetBattle::deployedShips_t deployedShipsB {std::make_pair("a1","a2"),std::make_pair("b1","b2"),std::make_pair("c1","c3"),std::make_pair("d1","d4"),std::make_pair("e1","e5")};
